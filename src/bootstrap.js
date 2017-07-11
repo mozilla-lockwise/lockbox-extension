adae40d421041f94f33fe4bcbb6fb8cb66918238
Jim Porter <jporter@mozilla.com>
2017-07-11 15:11:36 -0500

    Create an about:lockbox page and a toolbar button to open said page

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -2,12 +2,26 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+const {utils: Cu} = Components;
+
+let aboutFactory;
+
 function startup({webExtension}, reason) {
+  Cu.import("resource://lockbox/aboutPage.jsm");
+  aboutFactory = new ComponentFactory(AboutModule);
+
   webExtension.startup().then(() => {
     console.log("embedded webextension has started");
   });
 }
 
-function shutdown(data, reason) {}
+function shutdown(data, reason) {
+  if (reason == APP_SHUTDOWN) {
+    return;
+  }
+
+  aboutFactory.unregister();
+}
+
 function install(data, reason) {}
 function uninstall(data, reason) {}
