b73b3721c9e96b1a31cf00e40788a53522125075
Matthew A. Miller <linuxwolf@outer-planes.net>
2017-07-26 15:38:00 -0600

    trivial include of 🔒📦 DataStore

--- src/webextension/background.js
+++ src/webextension/background.js
@@ -2,7 +2,14 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+import DataStore from "lockbox-datastore";
+
+let ds = DataStore.create();
+
 function openLockbox() {
+  if (ds.opened) {
+    // do something?
+  }
   browser.tabs.create({url: browser.extension.getURL("manage.html")});
 }
 
