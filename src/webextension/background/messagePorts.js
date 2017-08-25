24f1b06646584279d877e2cfb3019338b55935e8
Jim Porter <jporter@mozilla.com>
2017-08-25 16:28:48 -0500

    Don't broadcast datastore messages to the sender of the initial request

--- src/webextension/background/messagePorts.js
+++ src/webextension/background/messagePorts.js
@@ -13,26 +13,28 @@ browser.runtime.onConnect.addListener((port) => {
   });
 });
 
-function broadcast(message) {
-  for (let p of ports)
-    p.postMessage(message);
+function broadcast(message, excludeSender = null) {
+  for (let p of ports) {
+    if (!excludeSender || p.sender.contextId !== excludeSender.contextId)
+      p.postMessage(message);
+  }
 }
 
-browser.runtime.onMessage.addListener(async function(message) {
+browser.runtime.onMessage.addListener(async function(message, sender) {
   switch (message.type) {
   case "add_entry": {
     const entry = await fakeStore.add(message.entry);
-    broadcast({type: "added_entry", entry});
+    broadcast({type: "added_entry", entry}, sender);
     return {entry};
   }
   case "update_entry": {
     const entry = await fakeStore.update(message.entry);
-    broadcast({type: "updated_entry", entry});
+    broadcast({type: "updated_entry", entry}, sender);
     return {entry};
   }
   case "remove_entry":
     await fakeStore.remove(message.id);
-    broadcast({type: "removed_entry", id: message.id});
+    broadcast({type: "removed_entry", id: message.id}, sender);
     return {};
   case "get_entry":
     return {entry: await fakeStore.get(message.id)};
