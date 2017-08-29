745f4df71d3784223714179c8dd096272003d383
Jim Porter <jporter@mozilla.com>
2017-08-29 13:17:30 -0500

    Use the latest version of lockbox-datastore

--- src/webextension/background/datastore.js
+++ src/webextension/background/datastore.js
@@ -3,23 +3,9 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import DataStore from "lockbox-datastore";
-import jose from "node-jose";
-
-const keys = {
-  "encrypted": "eyJhbGciOiJQQkVTMi1IUzI1NitBMTI4S1ciLCJraWQiOiJXa3hjLUUyY0h1SDdwS244cDFDdVdKUmd1SlFiMzhSZkZKdmk3Q3VJMVg4IiwicDJjIjo4MTkyLCJwMnMiOiJQRXVSRkR3dkJIbUZ1NjA1amdFUGtSU05KTkdranpEaEVBWTdoUHc2RW9rIiwiZW5jIjoiQTI1NkdDTSJ9.mF_TyKRmlV2yL1HqFMd8GWtAw94Kl6DOd9LX_Di1d_g8AqNIJLSVaQ.AUs8cZ7P42AeyGQ0.2Uw.PA0_79GRBG1NGhiLYlaKwQ"
-};
-
-let master = {
-  "kty": "oct",
-  "kid": "Wkxc-E2cHuH7pKn8p1CuWJRguJQb38RfFJvi7CuI1X8",
-  "k": "cmVzaW4gcGVjY2FkaWxsbyBjYXJ0YWdlIGNpcmN1bW5hdmlnYXRlIGFyaXRobWV0aWMgcmV2ZXJlbnRpYWw"
-};
-
-master = jose.util.base64url.decode(master.k).toString("utf8");
 
 const datastore = DataStore.create({
-  prompts: {unlock: () => master},
-  keys: keys.encrypted
+  prompts: {unlock: () => ""},
 });
 
 export default datastore;
