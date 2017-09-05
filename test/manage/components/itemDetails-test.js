fea2f49113e087b2e80cf1b0413eb7a64af52211
Jim Porter <jporter@mozilla.com>
2017-09-05 16:32:15 -0500

    Add a test for changing the selected item in <ItemDetails/>

--- test/manage/components/itemDetails-test.js
+++ test/manage/components/itemDetails-test.js
@@ -9,6 +9,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import sinon from "sinon";
 import sinonChai from "sinon-chai";
+import PropTypes from "prop-types";
 
 chai.use(sinonChai);
 
@@ -32,6 +33,13 @@ describe("<ItemDetails/>", () => {
         <ItemDetails saveLabel="save-item" deleteLabel="delete-item"
                      onSave={onSave} onDelete={onDelete}/>
       );
+    });
+
+    it("form fields unfilled", () => {
+      const formFields = wrapper.find("input");
+      expect(formFields.get(0).value).to.equal("");
+      expect(formFields.get(1).value).to.equal("");
+      expect(formFields.get(2).value).to.equal("");
     });
 
     it("onSave called", () => {
@@ -83,29 +91,73 @@ describe("<ItemDetails/>", () => {
       );
     });
 
+    it("form fields filled", () => {
+      const formFields = wrapper.find("input");
+      expect(formFields.get(0).value).to.equal(fields.title);
+      expect(formFields.get(1).value).to.equal(fields.username);
+      expect(formFields.get(2).value).to.equal(fields.password);
+    });
+
     it("onSave called", () => {
       wrapper.find('button[type="submit"]').simulate("submit");
       expect(onSave).to.have.been.calledWith(fields);
     });
 
     it("onSave called after editing", () => {
-      const fields = wrapper.find("input");
-      simulateTyping(fields.at(0), "new title");
-      simulateTyping(fields.at(1), "new username");
-      simulateTyping(fields.at(2), "new password");
-      wrapper.find('button[type="submit"]').simulate("submit");
-
       const updatedFields = {
         title: "new title",
         username: "new username",
         password: "new password",
       };
+
+      const formFields = wrapper.find("input");
+      simulateTyping(formFields.at(0), updatedFields.title);
+      simulateTyping(formFields.at(1), updatedFields.username);
+      simulateTyping(formFields.at(2), updatedFields.password);
+      wrapper.find('button[type="submit"]').simulate("submit");
+
       expect(onSave).to.have.been.calledWith(updatedFields);
     });
 
     it("onDelete called", () => {
       wrapper.find("button").not('[type="submit"]').simulate("click");
       expect(onDelete).to.have.been.calledWith();
+    });
+  });
+
+  describe("change selected item", () => {
+    const fields = {
+      title: "title",
+      username: "username",
+      password: "password",
+    };
+
+    beforeEach(() => {
+      onSave = sinon.spy();
+      onDelete = sinon.spy();
+      wrapper = mountWithL10n(
+        <ItemDetails fields={fields}
+                     saveLabel="save-item" deleteLabel="delete-item"
+                     onSave={onSave} onDelete={onDelete}/>
+      );
+    });
+
+    it("form fields updated", () => {
+      const formFields = wrapper.find("input");
+      expect(formFields.get(0).value).to.equal(fields.title);
+      expect(formFields.get(1).value).to.equal(fields.username);
+      expect(formFields.get(2).value).to.equal(fields.password);
+
+      const updatedFields = {
+        title: "new title",
+        username: "new username",
+        password: "new password",
+      };
+
+      wrapper.setProps({fields: updatedFields});
+      expect(formFields.get(0).value).to.equal(updatedFields.title);
+      expect(formFields.get(1).value).to.equal(updatedFields.username);
+      expect(formFields.get(2).value).to.equal(updatedFields.password);
     });
   });
 });
