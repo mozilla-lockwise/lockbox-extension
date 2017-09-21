e751bf81e0e576c75db0e338b96afca6982f42cf
Peter deHaan <peter@deseloper.com>
2017-09-21 08:18:09 -0700

    Add ESLint JSX a11y plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,21 +14,26 @@ module.exports = {
   },
   "extends": [
     "eslint:recommended",
+    "plugin:jsx-a11y/recommended",
     "plugin:mozilla/recommended",
     "plugin:react/recommended",
   ],
   "env": {
     "browser": true,
+    "es6": true,
     "node": true,
     "webextensions": true,
-    "es6": true,
   },
   "plugins": [
+    "jsx-a11y",
     "mozilla",
     "react",
     ...extraPlugins,
   ],
   "rules": {
+    "jsx-a11y/click-events-have-key-events": "warn",
+    "jsx-a11y/no-noninteractive-element-interactions": "warn",
+
     "comma-dangle": ["error", "always-multiline"],
     "curly": "error",
     "no-console": "warn",
