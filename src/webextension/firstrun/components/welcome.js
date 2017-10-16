334b4972eb9c2fd2055b9e8f9d93d76d29a02c6a
Matthew A. Miller <linuxwolf@outer-planes.net>
2017-10-16 13:07:30 -0600

    address code review comments

--- src/webextension/firstrun/components/welcome.js
+++ src/webextension/firstrun/components/welcome.js
@@ -14,10 +14,19 @@ export default function Welcome() {
         <h1>wELCOMe</h1>
       </Localized>
       <Localized id="welcome-intro">
-        <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Sed posuere consectetur est at lobortis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam porta sem malesuada magna mollis euismod. Cras mattis consectetur purus sit amet fermentum.</p>
+        <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Sed
+        posuere consectetur est at lobortis. Lorem ipsum dolor sit amet,
+        consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit. Etiam porta sem malesuada magna mollis euismod. Cras
+        mattis consectetur purus sit amet fermentum.</p>
       </Localized>
       <Localized id="welcome-feedback">
-        <p>Curabitur blandit tempus porttitor. Nulla vitae elit libero, a pharetra augue. Vestibulum id ligula porta felis euismod semper. Maecenas sed diam eget risus varius blandit sit amet non magna. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+        <p>Curabitur blandit tempus porttitor. Nulla vitae elit libero, a
+        pharetra augue. Vestibulum id ligula porta felis euismod semper.
+        Maecenas sed diam eget risus varius blandit sit amet non magna.
+        Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis
+        vestibulum. Maecenas sed diam eget risus varius blandit sit amet
+        non magna.</p>
       </Localized>
     </section>
   );
