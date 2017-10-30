d16574597bdbdd98a9d7e03fc97b0a1d6ccf1c4e
Matthew A. Miller <linuxwolf@outer-planes.net>
2017-10-30 14:55:16 -0600

    (WIP) login via FxA

--- src/webextension/background/authorization/index.js
+++ src/webextension/background/authorization/index.js
@@ -5,6 +5,9 @@
 const DEFAULT_CONFIG = "dev-latest";
 
 import UUID from "uuid";
+import jose from "node-jose";
+
+import configs from "./configs.json";
 
 export class Authorization {
   constructor({config = DEFAULT_CONFIG, info}) {
@@ -32,6 +35,73 @@ export class Authorization {
   get email() { return (this.info && this.info.email) || undefined; }
 
   async signIn(interactive = true) {
+    let cfg = configs[this.config];
+
+    // setup authorization URL
+    let params = new URLSearchParams();
+    let state, pkceCode;
+    state = await jose.util.randomBytes(32).toString("hex");
+
+    console.log(`redirect URI == ${browser.identity.getRedirectURL()}`);
+
+    params.set("response_type", "code");
+    params.set("client_id", cfg.client_id);
+    params.set("redirect_uri", cfg.redirect_uri);
+    params.set("scope", cfg.scopes.join(" "));
+    params.set("state", state);
+    if (cfg.pkce) {
+      pkceCode = jose.util.randomBytes(32).toString("hex");
+      let pkceChallenge = (new TextEncoder()).encode(pkceCode);
+      pkceChallenge = await jose.JWA.digest("SHA-256", pkceChallenge);
+      pkceChallenge = jose.util.base64url.encode(pkceChallenge);
+      params.set("code_challenge", pkceChallenge);
+      params.set("code_challenge_method", "S256");
+    }
+
+    let url, response;
+    url = `${cfg.oauth_uri}/authorization?${params}`;
+    response = await browser.identity.launchWebAuthFlow({
+      url,
+      interactive,
+    });
+    console.log(`authorization response: ${response}`);
+    url = new URL(response);
+    params = url.searchParams;
+    if (params.get("state") !== state) {
+      throw new Error("invalid oauth state");
+    }
+    let authCode = params.get("code");
+    if (!authCode) {
+      throw new Error("missing oauth code");
+    }
+
+    // exchange token
+    let request;
+    request = {
+      grant_type: "authorization_code",
+      code: authCode,
+      code_verifier: pkceCode,
+      client_id: cfg.client_id,
+    };
+    if (cfg.pkce) {
+      request.code_verifier = pkceCode;
+    } else {
+      request.client_secret = cfg.client_secret;
+    }
+    url = `${cfg.oauth_uri}/token`;
+    response = await fetch(url, {
+      method: "post",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: (new TextEncoder()).encode(JSON.stringify(request)),
+    });
+    let body = await response.json();
+    if (!response.ok) {
+      let err = new Error("oauth token error: ${body.message || response.statusText}");
+      err.code = body.errno;
+    }
+
     let uid = UUID();
     this.info = {
       uid,
