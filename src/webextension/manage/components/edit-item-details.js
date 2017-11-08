e5b1633f811789eeab86ec215f9ec13ffddf6ae3
Jim Porter <jporter@mozilla.com>
2017-11-08 14:25:12 -0600

    Add a doorhanger
    Much of this is still temporary, especially the directory structure, but the
    core pieces of the doorhanger actually work! Tests for the doorhanger will be
    added in a followup commit.

--- src/webextension/manage/components/edit-item-details.js
+++ src/webextension/manage/components/edit-item-details.js
@@ -7,11 +7,8 @@ import PropTypes from "prop-types";
 import React from "react";
 
 import Button from "../../widgets/button";
-import Input from "../../widgets/input";
-import PasswordInput from "../../widgets/password-input";
-import TextArea from "../../widgets/text-area";
 import Toolbar from "../../widgets/toolbar";
-import LabelText from "../../widgets/label-text";
+import { EditItemFields } from "./item-fields";
 
 import styles from "./item-details.css";
 
@@ -21,15 +18,8 @@ import styles from "./item-details.css";
 export default class EditItemDetails extends React.Component {
   static get propTypes() {
     return {
+      ...EditItemFields.propTypes,
       newItem: PropTypes.bool,
-      fields: PropTypes.shape({
-        title: PropTypes.string.isRequired,
-        origin: PropTypes.string.isRequired,
-        username: PropTypes.string.isRequired,
-        password: PropTypes.string.isRequired,
-        notes: PropTypes.string.isRequired,
-      }),
-      onChange: PropTypes.func.isRequired,
       onSave: PropTypes.func.isRequired,
       onCancel: PropTypes.func.isRequired,
     };
@@ -53,10 +43,6 @@ export default class EditItemDetails extends React.Component {
     this.state = {...props.fields};
   }
 
-  componentDidMount() {
-    this._firstField.focus();
-  }
-
   handleChange(event) {
     this.setState({[event.target.name]: event.target.value});
     this.props.onChange();
@@ -64,11 +50,6 @@ export default class EditItemDetails extends React.Component {
 
   render() {
     const {newItem, onSave, onCancel} = this.props;
-    const controlledProps = (name, maxLength = 500) => {
-      return {name, value: this.state[name],
-              onChange: (e) => this.handleChange(e),
-              maxLength: maxLength.toString()};
-    };
 
     return (
       <form className={`${styles.itemDetails} ${styles.editing}`}
@@ -79,51 +60,8 @@ export default class EditItemDetails extends React.Component {
         <Localized id={`item-details-heading-${newItem ? "new" : "edit"}`}>
           <h1>eDIt iTEm</h1>
         </Localized>
-        <label>
-          <Localized id="item-details-title">
-            <LabelText>tITLe</LabelText>
-          </Localized>
-          <Localized id="item-details-title-input">
-            <Input type="text" {...controlledProps("title")}
-                   placeholder="eNTRy nAMe"
-                   ref={(element) => this._firstField = element}/>
-          </Localized>
-        </label>
-        <label>
-          <Localized id="item-details-origin">
-            <LabelText>oRIGIn</LabelText>
-          </Localized>
-          <Localized id="item-details-origin-input">
-            <Input type="text" monospace={true}
-                   placeholder="wWw.eXAMPLe.cOm"
-                   {...controlledProps("origin")}/>
-          </Localized>
-        </label>
-        <label>
-          <Localized id="item-details-username">
-            <LabelText>uSERNAMe</LabelText>
-          </Localized>
-          <Localized id="item-details-username-input">
-            <Input type="text" monospace={true}
-                   placeholder="nAMe@eXAMPLE.cOm"
-                   {...controlledProps("username")}/>
-          </Localized>
-        </label>
-        <label>
-          <Localized id="item-details-password">
-            <LabelText>pASSWORd</LabelText>
-          </Localized>
-          <PasswordInput {...controlledProps("password")}/>
-        </label>
-        <label>
-          <Localized id="item-details-notes">
-            <LabelText>nOTEs</LabelText>
-          </Localized>
-          <Localized id="item-details-notes-input">
-            <TextArea placeholder="aNSWERs tO sECURITy..."
-                      {...controlledProps("notes", 10000)}/>
-          </Localized>
-        </label>
+        <EditItemFields fields={this.state}
+                        onChange={(e) => this.handleChange(e)}/>
         <Toolbar className={styles.buttons}>
           <Localized id={`item-details-save-${newItem ? "new" : "existing"}`}>
             <Button type="submit" theme="primary">sAVe</Button>
