6121a58534eadcf990bb07271eb523590a5b41f4
Matthew A. Miller <linuxwolf@outer-planes.net>
2017-11-29 17:54:12 -0700

    rework browser action to account for acount mode

--- src/webextension/background/index.js
+++ src/webextension/background/index.js
@@ -3,24 +3,31 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import openDataStore from "./datastore";
-import { loadAccount } from "./accounts";
+import getAccount, { loadAccount, GUEST } from "./accounts";
 import initializeMessagePorts from "./message-ports";
 import updateBrowserAction from "./browser-action";
 
 // XXX: For now, initialize the datastore on startup and then hook up the
 // button. Eventually, we'll have UX to create new datastores (and persist
 // existing ones).
-openDataStore().then(async (ds) => {
+openDataStore().then(async (datastore) => {
+  let account;
   try {
-    // attempt to load authorization (FxA) data
-    let acct = await loadAccount(browser.storage.local);
+    // attempt to load account (FxA) data
+    account = await loadAccount(browser.storage.local);
     // eslint-disable-next-line no-console
-    console.log(`loaded authorization for '${acct.uid || ""}'`);
+    console.log(`loaded authorization for (${account.mode.toString()}) '${account.uid || ""}'`);
   } catch (err) {
     // eslint-disable-next-line no-console
     console.error(`loading failed: ${err.message}`);
+    account = getAccount();
+  }
+
+  if (datastore.initialized && account.mode === GUEST) {
+    // start unlocked ...
+    await datastore.unlock();
   }
 
   initializeMessagePorts();
-  await updateBrowserAction(ds);
+  await updateBrowserAction({account, datastore});
 });
