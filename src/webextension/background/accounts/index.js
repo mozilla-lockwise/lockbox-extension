fe96a6650a1541426312ec9abdc0308c5334e217
Matthew A. Miller <linuxwolf@outer-planes.net>
2017-12-04 17:32:36 -0700

    use FxA prod

--- src/webextension/background/accounts/index.js
+++ src/webextension/background/accounts/index.js
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const DEFAULT_CONFIG = "scoped-keys";
+const DEFAULT_CONFIG = "production";
 
 import jose from "node-jose";
 
