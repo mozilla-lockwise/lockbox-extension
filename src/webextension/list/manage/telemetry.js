5c48f5405dc7e5951bd87ed6f00aa3a33c989720
Jim Porter <jporter@mozilla.com>
2017-12-05 16:38:13 -0600

    Add missing telemetry events (and send the item id across)

--- src/webextension/list/manage/telemetry.js
+++ src/webextension/list/manage/telemetry.js
@@ -10,19 +10,33 @@ export default (store) => (next) => (action) => {
   case actions.ADD_ITEM_STARTING:
     telemetry.recordEvent("itemAdding", "addItemForm");
     break;
-  case actions.ADD_ITEM_COMPLETED:
-    if (action.interactive) {
-      telemetry.recordEvent("itemAdded", "addItemForm");
-    }
-    break;
   case actions.UPDATE_ITEM_STARTING:
     telemetry.recordEvent("itemUpdating", "updatingItemForm");
     break;
   case actions.REMOVE_ITEM_STARTING:
     telemetry.recordEvent("itemDeleting", "updatingItemForm");
     break;
+  case actions.ADD_ITEM_COMPLETED:
+    if (action.interactive) {
+      telemetry.recordEvent("itemAdded", "addItemForm",
+                            {itemid: action.item.id});
+    }
+    break;
+  case actions.UPDATE_ITEM_COMPLETED:
+    if (action.interactive) {
+      telemetry.recordEvent("itemUpdated", "updatingItemForm",
+                            {itemid: action.item.id});
+    }
+    break;
+  case actions.REMOVE_ITEM_COMPLETED:
+    if (action.interactive) {
+      telemetry.recordEvent("itemDeleted", "updatingItemForm",
+                            {itemid: action.item.id});
+    }
+    break;
   case actions.SELECT_ITEM_COMPLETED:
-    telemetry.recordEvent("itemSelected", "itemList");
+    telemetry.recordEvent("itemSelected", "itemList",
+                          {itemid: action.item.id});
     break;
   case actions.START_NEW_ITEM:
     telemetry.recordEvent("addClick", "addButton");
