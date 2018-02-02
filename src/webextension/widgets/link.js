76a4932e0f126a17da2364f0af2ad718320a2dff
Jim Porter <jporter@mozilla.com>
2018-02-02 13:23:25 -0600

    Add a <Link/> widget and ensure that it (and <ExternalLink/>) are keyboard-accessible

--- src/webextension/widgets/link.js
+++ src/webextension/widgets/link.js
@@ -7,19 +7,42 @@ import React from "react";
 
 import styles from "./link.css";
 
-export function ExternalLink({onClick, children, className, ...props}) {
-  const finalClassName = `${styles.external} ${className}`.trimRight();
-  return (
-    <a {...props} className={finalClassName} onClick={onClick}>{children}</a>
-  );
+export class Link extends React.Component {
+  static get propTypes() {
+    return {
+      children: PropTypes.node,
+      className: PropTypes.string,
+      onClick: PropTypes.func.isRequired,
+    };
+  }
+
+  static get defaultProps() {
+    return {
+      className: "",
+    };
+  }
+
+  get baseClassName() {
+    return `${styles.link}`;
+  }
+
+  focus() {
+    this.linkElement.focus();
+  }
+
+  render() {
+    const {className, onClick, children, ...props} = this.props;
+    const finalClassName = `${this.baseClassName} ${className}`.trimRight();
+    return (
+      <a href="#" ref={(element) => this.linkElement = element}
+         {...props} className={finalClassName}
+         onClick={(e) => { onClick(e); e.preventDefault(); }}>{children}</a>
+    );
+  }
 }
 
-ExternalLink.propTypes = {
-  children: PropTypes.node,
-  className: PropTypes.string,
-  onClick: PropTypes.func.isRequired,
-};
-
-ExternalLink.defaultProps = {
-  className: "",
-};
+export class ExternalLink extends Link {
+  get baseClassName() {
+    return `${styles.link} ${styles.external}`;
+  }
+}
