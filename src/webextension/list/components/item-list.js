a75c4acf5222e91b54272a8139b9f92012cfe0dd
Nick Brandt <nbrandt.design@gmail.com>
2018-03-15 09:30:05 -0600

    Doorhanger entry list spacing and state style updates (#619)
    * Styling updates for doorhanger entry list
    
    -Moved styles for list items to .item class in order to propagate to both DH and full view list.
    -Increased width of dh to 320 to match designs and sign-in width
    -Added new item-list-panel css file to fixed panel header styling specific to that panel
    -Added default background for panel header
    
    * Changes based on discussion with Jim and Ryan
    
    -Updating panel width to be 280px for both entry list/details and unlock
    -Removing search header specific styling and updating panel default header to match for both entry list and detail
    -Removing wrapping toolbar from search in doorhanger
    -Updating some sizing for unlock content based on width change of panel
    
    * Updates based on PR review
    
    -Moving focus state for entry items into item-list.css and then removing unnecessary all-items.css file.
    -Moving styledItems from all-items.js to item-list.js to effect both instances of entry list
    -Bringing back in inline-end border for aside that was mistakenly removed
    -Bringing back in 'styled-item' styles that are currently not in use but may be needed in future.
    
    * Updating doorhanger caret color, modifying panel header padding/height, fixing entry focus state, and removing some unnecessary styles.
    
    * Updating hover/active/press/focus states of list items to occur on the item-summary element in order to account for areas where we have quick copy links available.
    
    * Moving the position definition for item-summary to the selected state.
    
    * Add unit test for className in <ItemSummary/>

--- src/webextension/list/components/item-list.js
+++ src/webextension/list/components/item-list.js
@@ -18,10 +18,10 @@ export default function ItemList({items, itemClassName, verbose, onCopy,
   ).trimRight();
 
   return (
-    <ScrollingList itemClassName={finalItemClassName} data={items} {...props}>
+    <ScrollingList className={styles.itemList} itemClassName={finalItemClassName} data={items} styledItems={false} {...props}>
       {({id, title, username}) => {
         return (
-          <ItemSummary id={id} title={title} username={username}
+          <ItemSummary className={styles.itemSummary} id={id} title={title} username={username}
                        verbose={verbose} onCopy={onCopy}/>
         );
       }}
