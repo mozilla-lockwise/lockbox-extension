a8fd01b5efdf8435a158fb3432bf3350deb38c50
Matt Brandt <mbrandt@mozilla.com>
2018-03-16 11:19:32 -0600

    udate create new entry method and move dh test to logical suite

---
+++ test/integration/tests/test_doorhanger.py
@@ -0,0 +1,13 @@
+"""Tests for the door hanger """
+
+
+def test_door_hanger_interaction(fxa_account, login_page):
+    """Add an entry and test it shows in the door hanger."""
+    fxa = fxa_account
+    home_page = login_page.sign_in(fxa.email, fxa.password)
+    home_page.create_new_entry("Tuna, Inc.", "https://satuna.org", "tuna4life",
+                               "tunafish", "The tuna swim at midnight")
+    lists = home_page.door_hanger.find_entrys()
+    assert 'Tuna, Inc.' in lists[0].title
+    entry = lists[0].click()
+    assert 'Tuna, Inc.' in entry.title
