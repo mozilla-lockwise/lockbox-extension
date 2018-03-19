ecc38569f653152a8d7eac6a9deccf6e42f79405
Matt Brandt <mbrandt@mozilla.com>
2018-03-19 15:44:59 -0600

    Improve DH integration tests and reorganize test suite (#666)
    * update create new entry method and move dh test to logical suite
    
    * move guest mode & fxa tests to new suites
    
    * remedy flake8 linting errors

--- test/integration/pages/home.py
+++ test/integration/pages/home.py
@@ -27,8 +27,6 @@ class Home(Base):
                           'section menu '
                           'button.{}:nth-child(1)'.format(
                             munged_class_name('button')))
-    _save_entry_locator = (By.CSS_SELECTOR, 'article div form menu '
-                           'button.{}'.format(munged_class_name('button')))
     _sign_in_locator = (By.CSS_SELECTOR, '.{} .{}'.format(
                         munged_class_name('link'),
                         munged_class_name('puffy-size')))
@@ -50,11 +48,18 @@ class Home(Base):
         """Lockie image locator."""
         return self.find_element(*self._lockie_locator).text
 
-    def add_entry(self):
-        """Add an entry into the lockbox."""
+    def create_new_entry(self, site_name='', url='', username='',
+                         password='', note=''):
+        """Create and save a new entry."""
         current_entries = len(self.entries)
         self.find_element(*self._new_entry_locator).click()
-        self.find_element(*self._save_entry_locator).click()
+        entry = self.entries[0].click()
+        entry.set_site_name(site_name)
+        entry.set_website(url)
+        entry.set_username(username)
+        entry.set_password(password)
+        entry.set_note(note)
+        entry.save()
         self.wait.until(lambda _: len(self.entries) != current_entries)
 
     def delete_entry(self):
@@ -117,12 +122,55 @@ class Entry(Region):
                           munged_class_name('first-label')))
         _title_text_locator = (By.CLASS_NAME, '{}'.format(
                                munged_class_name('field-text')))
+        _site_name_locator = (By.CSS_SELECTOR, 'article div form '
+                              'input[name="title"]')
+        _website_address_locator = (By.CSS_SELECTOR, 'article div form '
+                                    'input[name="origin"]')
+        _username_locator = (By.CSS_SELECTOR, 'article div form '
+                             'input[name="username"]')
+        _password_locator = (By.CSS_SELECTOR, 'article div form '
+                             'input[name="password"]')
+        _note_locator = (By.CSS_SELECTOR, 'article div form '
+                         'textarea[name="notes"]')
+        _save_entry_locator = (By.CSS_SELECTOR, 'article div form menu'
+                               'button.{}'.format(munged_class_name('button')))
+        _save_entry_locator = (By.CSS_SELECTOR,
+                               'article div form menu button[type="submit"]')
+
+        def set_site_name(self, site_name):
+            """Set the entry site name."""
+            field = self.find_element(*self._site_name_locator)
+            field.send_keys(site_name)
+
+        def set_website(self, url):
+            """Set the entry website."""
+            field = self.find_element(*self._website_address_locator)
+            field.send_keys(url)
+
+        def set_username(self, username):
+            """Set the entry username."""
+            field = self.find_element(*self._username_locator)
+            field.send_keys(username)
+
+        def set_password(self, password):
+            """Set the entry password."""
+            field = self.find_element(*self._password_locator)
+            field.send_keys(password)
+
+        def set_note(self, note):
+            """Set the entry note."""
+            field = self.find_element(*self._note_locator)
+            field.send_keys(note)
 
         @property
         def title(self):
             """Entry title."""
             title = self.find_element(*self._title_locator)
             return title.find_element(*self._title_text_locator).text
+
+        def save(self):
+            """Save the entry."""
+            self.find_element(*self._save_entry_locator).click()
 
         def delete(self):
             """Delete an entry from the lockbox."""
