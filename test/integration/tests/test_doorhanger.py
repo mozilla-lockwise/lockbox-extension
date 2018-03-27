1a13c1d01da46cc582842f9b44c4095aabf361e5
Kimberly <ksereduck@mozilla.com>
2018-03-27 10:49:29 -0600

    adding accessibility tests

--- test/integration/tests/test_doorhanger.py
+++ test/integration/tests/test_doorhanger.py
@@ -7,6 +7,7 @@ def test_door_hanger_interaction(fxa_account, login_page):
     home_page = login_page.sign_in(fxa.email, fxa.password)
     home_page.create_new_entry("Tuna, Inc.", "https://satuna.org", "tuna4life",
                                "tunafish", "The tuna swim at midnight")
+
     lists = home_page.door_hanger.find_entrys()
     assert 'Tuna, Inc.' in lists[0].title
     entry = lists[0].click()
