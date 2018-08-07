0aa2bc1899b5221818a99d2f1887f7a95983693c
Devin Reams <dreams@mozilla.com>
2018-08-07 10:35:49 -0600

    add big disclaimer to settings page

--- src/webextension/settings/components/app.js
+++ src/webextension/settings/components/app.js
@@ -2,6 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+import { Localized } from "fluent-react";
 import React from "react";
 
 import LocalReset from "./local-reset";
@@ -12,6 +13,9 @@ import "./app.css";
 export default function App() {
   return (
     <article>
+      <Localized id="settings-disclaimer">
+        <h1>nOTe: tHIs eXTENSION Is nOt mAINTAINEd</h1>
+      </Localized>
       <LocalReset/>
       <ModalRoot/>
     </article>
